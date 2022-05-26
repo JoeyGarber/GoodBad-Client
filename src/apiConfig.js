22dfac0d5d5fbcb7c752aa3939eaa72a67807b20
JoeyGarber <joseph.z.garber@gmail.com>
2022-05-26 14:48:36 -0400

    Setting up production database

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://vast-reaches-13226.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
